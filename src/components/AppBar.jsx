bc3fd42af039002b4600f29d6f9474d8f4e85cdc
sambbaahh <samikukkonen00@hotmail.com>
2024-03-15 12:56:30 +0200

    10.6-10.7 done

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,6 @@
-import { View, StyleSheet, Text, Pressable } from "react-native";
+import { View, StyleSheet, Text, Pressable, ScrollView } from "react-native";
 import Constants from "expo-constants";
+import { Link } from "react-router-native";
 
 const styles = StyleSheet.create({
   container: {
@@ -9,17 +10,36 @@ const styles = StyleSheet.create({
   text: {
     color: "white",
     fontSize: 20,
+  },
+  scrollView: {
+    display: "flex",
+    flexDirection: "row",
     paddingVertical: 20,
-    paddingHorizontal: 20,
+    paddingHorizontal: 15,
+    gap: 15,
+    flexGrow: 1,
   },
 });
+
+const AppBarTab = (props) => (
+  <Pressable>
+    <Link to={props.route}>
+      <Text style={styles.text}>{props.title}</Text>
+    </Link>
+  </Pressable>
+);
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Pressable onPress={() => console.log("test press")}>
-        <Text style={styles.text}>Repositories</Text>
-      </Pressable>
+      <ScrollView
+        horizontal
+        contentContainerStyle={styles.scrollView}
+        scrollEnabled
+      >
+        <AppBarTab title={"Repositories"} route={"/"} />
+        <AppBarTab title={"Sign in"} route={"/sign-in"} />
+      </ScrollView>
     </View>
   );
 };
