d1423213b9bf528a799859e178a9b8edc01560b1
sambbaahh <samikukkonen00@hotmail.com>
2024-03-18 21:56:26 +0200

    10.23 done

--- src/components/RepostitoryList.jsx
+++ src/components/RepostitoryList.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-native";
 
 import RepositoryItem from "./RepositoryItem";
 import { GET_REPOSITORIES } from "../graphql/queries";
+import { Picker } from "@react-native-picker/picker";
+import { useState } from "react";
 
 const styles = StyleSheet.create({
   separator: {
@@ -13,14 +15,53 @@ const styles = StyleSheet.create({
 
 export const ItemSeparator = () => <View style={styles.separator} />;
 
+const SortPicker = (props) => (
+  <Picker {...props}>
+    <Picker.Item
+      label={"Select an item..."}
+      enabled={false}
+      style={{ color: "gray" }}
+    />
+    <Picker.Item
+      label="Latest repositories"
+      value={JSON.stringify({ orderBy: "CREATED_AT", orderDirection: "DESC" })}
+    ></Picker.Item>
+    <Picker.Item
+      label="Highest rated repositories"
+      value={JSON.stringify({
+        orderBy: "RATING_AVERAGE",
+        orderDirection: "DESC",
+      })}
+    ></Picker.Item>
+    <Picker.Item
+      label="Lowest rated repositories"
+      value={JSON.stringify({
+        orderBy: "RATING_AVERAGE",
+        orderDirection: "ASC",
+      })}
+    ></Picker.Item>
+  </Picker>
+);
+
 const RepositoryList = () => {
   const navigate = useNavigate();
+  const [order, setOrder] = useState(
+    JSON.stringify({ orderBy: "CREATED_AT", orderDirection: "DESC" })
+  );
+
+  const changeOrder = (itemValue) => {
+    setOrder(itemValue);
+  };
 
   const handleNavigate = (id) => {
     navigate(`/repository/${id}`);
   };
 
   const { data, loading } = useQuery(GET_REPOSITORIES, {
+    variables: {
+      orderBy: `${JSON.parse(order).orderBy}`,
+      orderDirection: `${JSON.parse(order).orderDirection}`,
+    },
     fetchPolicy: "cache-and-network",
   });
 
@@ -36,6 +77,13 @@ const RepositoryList = () => {
           <RepositoryItem {...item} />
         </Pressable>
       )}
+      ListHeaderComponent={
+        <SortPicker
+          selectedValue={order}
+          onValueChange={changeOrder}
+          placeholder={{ label: "select.." }}
+        />
+      }
     />
   );
 };
